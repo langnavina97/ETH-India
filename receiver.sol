6de4af35492da7671edf31d7646b8be569bd575d
langnavina97 <81016364+langnavina97@users.noreply.github.com>
2022-12-04 08:40:19 +0530

    bugfix receiver

--- receiver.sol
+++ receiver.sol
@@ -4,6 +4,8 @@ import {IConnext} from "@connext/nxtp-contracts/contracts/core/connext/interface
 import {IXReceiver} from "@connext/nxtp-contracts/contracts/core/connext/interfaces/IXReceiver.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+import "../interfaces/IUniswapV2Router02.sol";
+
 import "./IIndexSwap.sol";
 import "./IWETH.sol";
 
@@ -17,10 +19,13 @@ contract ReceiverIndex is IXReceiver {
   // The connext contract deployed on the same domain as this contract
   IConnext public immutable connext;
 
+  IUniswapV2Router02 uniswapRouter;
 
-  constructor(IConnext _connext, IIndexSwap _index) {
+
+  constructor(IConnext _connext, IIndexSwap _index, address _router) {
     connext = _connext;
     index = _index;
+    uniswapRouter = IUniswapV2Router02(_router);
   }
 
   /** 
@@ -39,15 +44,31 @@ contract ReceiverIndex is IXReceiver {
     // Ping's address was sent with the xcall so it could be decoded and used for the nested xcall
     (address user) = abi.decode(_callData, (address));
 
+    // weth balance
     IERC20 _token = IERC20(_asset);
     uint256 balance = _token.balanceOf(address(this));
 
-    IWETH weth = IWETH(_asset);
-    weth.withdraw(balance);
+    // swap to native token
+    uniswapRouter.swapExactTokensForETH(
+        balance,
+        1,
+        getPathForToken(_asset),
+        address(this),
+        block.timestamp
+    )[1];
 
-    // IIndexSwap index = IIndexSwap(_index);
-
-    index.investInFund{value: balance} (user);
+    index.investInFund{value: address(this).balance} (user);
     
   }
+
+  function getPathForToken(address token)
+        public
+        view
+        returns (address[] memory)
+    {
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = uniswapRouter.WETH();
+        return path;
+    }
 }
